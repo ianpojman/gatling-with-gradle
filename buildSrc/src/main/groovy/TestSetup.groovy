d62b4ba2155edc8f194c760ef8c7dad417c7e01d
Andrew Grauch <agrauch@agrauch-mbr.f4tech.com>
2014-04-04 13:22:38 -0600

    more comments

--- buildSrc/src/main/groovy/TestSetup.groovy
+++ buildSrc/src/main/groovy/TestSetup.groovy
@@ -6,5 +6,6 @@ class TestSetup extends DefaultTask {
     @TaskAction
     def setup() {
         // TODO: setup test data using web service calls or directly with SQL
+        // TODO: generate feeder files for Gatling
     }
 }
